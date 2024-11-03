2fa34507ea1ea7746db9ebbdc39d422b34160d48
Meghana0204 <151699243+Meghana0204@users.noreply.github.com>
2024-11-04 03:07:48 +0530

    Add files via upload

---
+++ ibm ml project/app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, render_template # type: ignore
+app = Flask(__name__)
+# Load the pickled model
+with open('fraud_detection_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+ 
+@app.route('/')
+def index():
+    
+    return render_template('index.html')
+
+@app.route('/predict_fraud', methods=['POST'])
+def predict_fraud(): 
+        if request.method =='POST':
+            type = int(request.form.get('type'))
+            amount = float(request.form.get('amount'))
+            oldbalanceOrg = float(request.form.get('oldbalanceOrg'))
+            newbalanceOrig = float(request.form.get('newbalanceOrig'))
+        
+   
+        prediction = model.predict([[type,amount,oldbalanceOrg,newbalanceOrig]])
+
+        # Return the prediction result
+        
+        return render_template('predict_fraud.html', is_fraud=prediction[0])
+
+   
+if __name__ == '__main__':
+    app.run(debug=True)
